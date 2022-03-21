8e0210276eb9bb063bd63e1f7d5457d8c24011e8
Nandagopal T <gopikrish.thiru@gmail.com>
2022-03-21 23:16:08 +0530

    Practice | TMDB Application with MVVM Architecture Part 7 | Covering the components for Presentation layer

--- TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/di/DatabaseModule.kt
+++ TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/di/DatabaseModule.kt
@@ -8,24 +8,30 @@ import com.amalwin.tmdbretrofitcoroutinesexperiment.data.db.TMDBDatabase
 import com.amalwin.tmdbretrofitcoroutinesexperiment.data.db.TVShowDAO
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+    @Singleton
     @Provides
     fun provideDatabase(context: Context): TMDBDatabase {
         return Room.databaseBuilder(context, TMDBDatabase::class.java, "TMDBDatabase").build()
     }
 
+    @Singleton
     @Provides
     fun provideMovieDAO(tmdbDatabase: TMDBDatabase): MovieDAO {
         return tmdbDatabase.getMovieDAO()
     }
 
+    @Singleton
     @Provides
     fun provideTVShowDAO(tmdbDatabase: TMDBDatabase): TVShowDAO {
         return tmdbDatabase.getTVShowDAO()
     }
 
+    @Singleton
+    @Provides
     fun provideActorDAO(tmdbDatabase: TMDBDatabase): ActorDAO {
         return tmdbDatabase.getActorDAO()
     }
---
+++ TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/di/RemoteDataModule.kt
@@ -0,0 +1,35 @@
+package com.amalwin.tmdbretrofitcoroutinesexperiment.presentation.di
+
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.api.TMDBService
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasource.ActorRemoteDataSource
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasource.MovieRemoteDataSource
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasource.TVShowRemoteDataSource
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasourceimpl.ActorRemoteDataSourceImpl
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasourceimpl.MovieRemoteDataSourceImpl
+import com.amalwin.tmdbretrofitcoroutinesexperiment.data.repository.datasourceimpl.TVShowRemoteDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RemoteDataModule(private val apiKey: String) {
+
+    @Singleton
+    @Provides
+    fun provideMovieDataSource(tmdbService: TMDBService): MovieRemoteDataSource {
+        return MovieRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTVShowDataSource(tmdbService: TMDBService): TVShowRemoteDataSource {
+        return TVShowRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+
+    @Singleton
+    @Provides
+    fun provideActorDataSource(tmdbService: TMDBService): ActorRemoteDataSource {
+        return ActorRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+
+}
