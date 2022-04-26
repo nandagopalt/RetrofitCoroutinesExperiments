c508dec01fabded019c11fc45670044bca0f4325
Nandagopal T <gopikrish.thiru@gmail.com>
2022-04-26 19:52:55 +0530

    Practice | Adding the adapter recyclerview code for Movie and Actor Module + Updating the respective activities accordingly

--- TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/actor/ActorActivity.kt
+++ TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/actor/ActorActivity.kt
@@ -1,17 +1,52 @@
 package com.amalwin.tmdbretrofitcoroutinesexperiment.presentation.actor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amalwin.tmdbretrofitcoroutinesexperiment.R
 import com.amalwin.tmdbretrofitcoroutinesexperiment.databinding.ActivityActorBinding
+import com.amalwin.tmdbretrofitcoroutinesexperiment.presentation.di.core.Injector
+import javax.inject.Inject
 
 class ActorActivity : AppCompatActivity() {
+    @Inject
+    lateinit var actorViewModelFactory: ActorViewModelFactory
+    private lateinit var actorViewModel: ActorViewModel
+    private lateinit var actorAdapter: ActorAdapter
     private lateinit var binding: ActivityActorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_actor)
         //setContentView(R.layout.activity_actor)
         binding.actorTitle.text = "Actor"
+        (application as Injector).createActorSubComponent().inject(this)
+        actorViewModel = ViewModelProvider(this, actorViewModelFactory)[ActorViewModel::class.java]
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        binding.actorRecyclerView.layoutManager = LinearLayoutManager(this)
+        actorAdapter = ActorAdapter()
+        binding.actorRecyclerView.adapter = actorAdapter
+        fetchActorList()
+    }
+
+    fun fetchActorList() {
+        binding.actorRecyclerView.visibility = View.VISIBLE
+        val actorsLiveData = actorViewModel.getActors()
+        actorsLiveData.observe(this) { actors ->
+            if (actors != null) {
+                actorAdapter.setActorList(actors)
+                actorAdapter.notifyDataSetChanged()
+                binding.actorProgressBar.visibility = View.GONE
+            } else {
+                binding.actorProgressBar.visibility = View.GONE
+                Toast.makeText(this, "No data found !", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/tvshow/TVShowActivity.kt
+++ TMDBRetrofitCoroutinesExperiment/app/src/main/java/com/amalwin/tmdbretrofitcoroutinesexperiment/presentation/tvshow/TVShowActivity.kt
@@ -2,16 +2,53 @@ package com.amalwin.tmdbretrofitcoroutinesexperiment.presentation.tvshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amalwin.tmdbretrofitcoroutinesexperiment.R
 import com.amalwin.tmdbretrofitcoroutinesexperiment.databinding.ActivityTvshowBinding
+import com.amalwin.tmdbretrofitcoroutinesexperiment.presentation.di.core.Injector
+import javax.inject.Inject
 
 class TVShowActivity : AppCompatActivity() {
+    @Inject
+    lateinit var tvShowViewModelFactory: TVShowViewModelFactory
+    private lateinit var tvShowViewModel: TVShowViewModel
     private lateinit var binding: ActivityTvshowBinding
+    private lateinit var tvShowAdapter: TVShowAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_tvshow)
         //setContentView(R.layout.activity_tvshow)
         binding.tvShowTitle.text = "TV Show"
+        (application as Injector).createTVShowSubComponent().inject(this)
+        tvShowViewModel =
+            ViewModelProvider(this, tvShowViewModelFactory)[TVShowViewModel::class.java]
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        binding.tvShowRecyclerView.layoutManager = LinearLayoutManager(this)
+        tvShowAdapter = TVShowAdapter()
+        binding.tvShowRecyclerView.adapter = tvShowAdapter
+        fetchTVShowList()
+    }
+
+    private fun fetchTVShowList() {
+        binding.tvShowProgressBar.visibility = View.VISIBLE
+        val tvShowLiveData = tvShowViewModel.getTVShows()
+        tvShowLiveData.observe(this) { tvShow ->
+            if (tvShow != null) {
+                tvShowAdapter.setTVShowList(tvShow)
+                tvShowAdapter.notifyDataSetChanged()
+                binding.tvShowProgressBar.visibility = View.GONE
+            } else {
+                binding.tvShowProgressBar.visibility = View.GONE
+                Toast.makeText(this, "No data found !", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
